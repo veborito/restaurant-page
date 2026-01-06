efb146b23bdf2219c4896e1837aa12ce62fef560
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-01-06 21:13:29 +0100

    feat: home page description and restaurantName

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 import "./styles.css";
+import { footer } from "./footer.js"
 
-const body = document.querySelector("body");
-const footer = document.createElement("footer");
-const unsplashCredit = document.createElement("p");
-const link = document.createElement("a");
+function sameClassElement(containers, className) {
+  for (let container of containers) {
+    container.className = className;
+  }
+}
 
-link.textContent = "@edgarraw";
-link.href = "https://unsplash.com/fr/@edgarraw"
-unsplashCredit.textContent = "Background from:";
-unsplashCredit.appendChild(link);
-footer.appendChild(unsplashCredit);
-body.appendChild(footer);
+function createParagraph(container, text) {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = text;
+  container.appendChild(paragraph);
+}
+
+function addContent(content, element) {
+  content.appendChild(element);
+}
+
+const content = document.getElementById("content");
+const restaurantName = document.createElement("h1");
+const description = document.createElement("div");
+const openHours = document.createElement("div");
+const descriptionText = `
+  Hi, we are a family that cooks swiss, cuban dishes.
+  We'd love to help you with your corean barbecue! 
+  We cook for our clients with pleasure (and difficulty) the finest japanese delicates.
+  Our baker is the best in town! Don't miss the opportunity to eat our 
+  delicious morning bowls!
+`;
+restaurantName.textContent = "Carmelle's kitchen";
+
+sameClassElement([description, openHours], "container");
+createParagraph(description, descriptionText);
+
+addContent(content, restaurantName);
+addContent(content, description);
+addContent(content, openHours);
+
+
+footer();
+
+
 
