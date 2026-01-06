cf5f5d6b58dbca37dd9ddac4590a684e068b9c28
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-01-06 21:12:55 +0100

    feat: footer module

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+const footer = () => {
+  const body = document.querySelector("body");
+  const footer = document.createElement("footer");
+  const unsplashCredit = document.createElement("p");
+  const link = document.createElement("a");
+
+  link.textContent = "@edgarraw";
+  link.href = "https://unsplash.com/fr/@edgarraw"
+  unsplashCredit.textContent = "Background from ";
+  unsplashCredit.appendChild(link);
+  footer.appendChild(unsplashCredit);
+  body.appendChild(footer);
+}
+
+export { footer };
