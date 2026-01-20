3eaf604c2ace49d642d2f266c8681946950a8e51
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-01-20 18:29:44 +0100

    fix: webpack image loading

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,23 @@
 import "./styles.css";
 import "./utils.js";
+import proteinBowlImage from "../assets/protein-bowl.jpg"
 import { addContent, addMultipleContent, createParagraph } from "./utils.js";
 
 function createMenuItem(imagePath, imageDescription,text, price) {
   const container = document.createElement("div");
   const imageAndDesc = document.createElement("div");
-  const image = new Image(100, 100);
+  const image = document.createElement("img");
   
   container.className = "menu-item";
 
+  image.className = "menu-img";
   image.src = imagePath;
   image.alt = imageDescription;
 
   createParagraph(imageAndDesc, text);
-  createParagraph(container, price);
-
   addContent(imageAndDesc, image);
   addContent(container, imageAndDesc);
+  createParagraph(container, price);
 
   return container;
 }
@@ -26,7 +27,7 @@ const menu = () => {
   const addedContent = document.createElement("div");
   const menuTitle = document.createElement("h1");
 
-  const item = createMenuItem("./assets/protein-bowl.jpg", "protein bowl","Extase in front of the most beautiful and colorfull protein bowl in town", "20 CHF");
+  const item = createMenuItem(proteinBowlImage, "protein bowl","Extase in front of the most beautiful and colorfull protein bowl in town", "20 CHF");
 
   addContent(addedContent, item);
   addContent(content, addedContent);
