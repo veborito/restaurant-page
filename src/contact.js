5e6ccb2985399f50d16a5b660039d8d7239cc17b
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-01-29 09:50:44 +0100

    feat: contact info

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,19 @@
 import "./styles.css";
+import { addContent, addMultipleContent } from "./utils.js";
 
 const contact = () => {
-  alert("This is the contact page");
+  const content = document.getElementById("content");
+  const addedContent = document.createElement("div");
+  const email = document.createElement("p");
+  const phone = document.createElement("p");
+  const location = document.createElement("p");
+
+  email.textContent = "Email: carmelleresto@tuconnais.com";
+  phone.textContent = "Phone: 0800 900 300 2023";
+  location.textContent = "Location: Bratislava";
+
+  addMultipleContent(addedContent, [email, phone, location]);
+  addContent(content, addedContent);
 }
 
 export { contact };
