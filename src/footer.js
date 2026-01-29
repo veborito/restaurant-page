c0d2b441046d21f25cf1defa1f6de27401fe6b19
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-01-29 09:42:27 +0100

    feat: new mention for images artists

--- src/footer.js
+++ src/footer.js
@@ -1,13 +1,28 @@
+
+const links = [
+  "https://unsplash.com/fr/@edgarraw",
+  "https://unsplash.com/fr/@picoftasty",
+  "https://unsplash.com/fr/@pwign",
+]
+
+function createLinks(links, parent) {
+  for (let link of links) {
+    const newLink = document.createElement("a");
+    let artist_id = link.split('/').at(-1);
+    newLink.textContent = artist_id + " ";
+    newLink.href = link;
+    parent.appendChild(newLink);
+  }
+}
+
 const footer = () => {
   const body = document.querySelector("body");
   const footer = document.createElement("footer");
   const unsplashCredit = document.createElement("p");
-  const link = document.createElement("a");
 
-  link.textContent = "@edgarraw";
-  link.href = "https://unsplash.com/fr/@edgarraw"
-  unsplashCredit.textContent = "Background from ";
-  unsplashCredit.appendChild(link);
+  
+  unsplashCredit.textContent = "Images from ";
+  createLinks(links, unsplashCredit);
   footer.appendChild(unsplashCredit);
   body.appendChild(footer);
 }
