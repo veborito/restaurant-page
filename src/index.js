56f34a80043fcaf060df5d0b96f8be3443ed3d5b
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-02-20 22:20:22 +0100

    feat: package.json and webpack basic config

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
 import "./styles.css";
-import { footer } from "./footer.js";
-import { home } from "./home.js";
-import { menu } from "./menu.js";
-import { contact } from "./contact.js";
 
-const content = document.getElementById("content");
-
-function addButtonPage(button, func) {
-  button.addEventListener("click", () => {
-    content.textContent = "";
-    func();
-  })
-}
-
-const homeBtn = document.getElementById("home");
-const menuBtn = document.getElementById("menu");
-const contactBtn = document.getElementById("contact");
-
-const btnPageList = [
-  [homeBtn, home],
-  [menuBtn, menu],
-  [contactBtn, contact],
-]
-
-for (let [btn, func] of btnPageList) {
-  addButtonPage(btn, func);
-}
-
-home();
-footer();
+console.log("template");
